d73b62462307c2eda2a53c728b58efc905355d73
Matthew Wong <mattckwong@gmail.com>
2026-02-19 22:22:58 -0500

    chore: enable avenu server deployment + docs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || '/'}>
     <App />
   </BrowserRouter>
 );
